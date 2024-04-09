43cc44e9b691c685b2b93960d35447f38ddf0aee
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-04-09 10:28:28 +0200

    Create Car.cs

---
+++ Destructory/Destructor_1/Car.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Destructory_1.Classes
+{
+    internal class Car
+    {
+        private string name;
+        private double speed;
+
+        public Car(string name, double speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+
+        Car() 
+        {
+            Console.WriteLine($"Samochód {name} został zniszczony.");
+        }
+
+        public void StartRace()
+        {
+            Console.WriteLine($"Samochód {name} rozpoczął wyścig.");
+        }
+    }
+}
