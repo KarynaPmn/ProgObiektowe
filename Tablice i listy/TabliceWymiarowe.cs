8e4186e9771fe63874a7f4a1b760d2b7cc89caf9
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-06-19 18:03:34 +0200

    Delete Tablice i listy/TabliceWymiarowe.cs

--- Tablice i listy/TabliceWymiarowe.cs
+++
@@ -1,7 +0,0 @@
-// Przepisać z lekcji
-int[,] ints = new int[4, 3];
-Console.Readline(ints.Rank); // 2
-Console.Readline(ints.GetLenght(0)); // 4
-Console.Readline(ints.GetLenght(1)); // 3
-
-Console.Readline();
