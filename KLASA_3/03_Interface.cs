9b8c32a80cd10a9e6efd83a23616707c04292b1e
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-10-21 15:52:26 +0200

    Update 03_Interface.cs

--- KLASA_3/03_Interface.cs
+++ KLASA_3/03_Interface.cs
@@ -9,58 +9,66 @@ namespace Interface
     public interface IAnimal
     {
         void MakeSound();
-
         void Eat();
     }
-
     public abstract class Animal : IAnimal
     {
         public string Name { get; set; }
-        public int Age { get; set; }    
+        public int Age { get; set; }
         public Animal(string name, int age)
         {
             Name = name;
             Age = age;
         }
-
         public abstract void MakeSound();
         public void Eat()
         {
             Console.WriteLine($"{Name} je");
         }
-    }
-
-    public class Dog : Animal
-    {
-        public Dog(string name, int age) : base(name, age) { }
-        public override void MakeSound()
+        public class Dog : Animal
         {
-            Console.WriteLine("Gau!");
+            public Dog(string name, int age) : base(name, age) { }
+            public override void MakeSound()
+            {
+                Console.WriteLine("Gau!");
+            }
         }
 
-        public override void Eat();
-    }
-
-    public class Cat : Animal
-    {
-        public Cat(string name, int age) : base(name, age) { }
-        public override void MakeSound()
+        public class Cat : Animal
         {
-            Console.WriteLine("Miau!");
+            public Cat(string name, int age) : base(name, age) { }
+            public override void MakeSound()
+            {
+                Console.WriteLine("Meow!");
+            }
         }
-
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
+        internal class Program
         {
-            Dog dog = new Dog();
-            Cat cat = new Cat();
+            static void Main(string[] args)
+            {
+                Dog dog = new Dog("Azor", 3);
+                Cat cat = new Cat("Mruczek", 2);
 
-            dog.MakeSound();
-            cat.MakeSound();
+                dog.MakeSound();
+                cat.MakeSound();
 
-            Console.ReadKey();
+                dog.Eat();
+                cat.Eat();
+                Console.Clear();
+
+                var Animal = new List<Animal>()
+                {
+                    dog,cat,
+                    new Dog("Azor", 3),
+                    new Cat("Mruczek", 2),
+                };
+                foreach (var animal in Animal)
+                {
+                    animal.MakeSound();
+                    animal.Eat();
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
