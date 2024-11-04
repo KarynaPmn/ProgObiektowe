712d472a45ab443fb8deba14bbf6931da0873460
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-11-04 15:35:12 +0100

    Update 04_InterfaceBiblioteka.cs

--- KLASA_3/04_InterfaceBiblioteka.cs
+++ KLASA_3/04_InterfaceBiblioteka.cs
@@ -19,3 +19,73 @@
 //   System.Linq. Wyświetl posortowane listy na konsoli, używając metody ToString() klasy
 //   Book.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Interface
+{
+    class Book : IComparable<Book>
+    {
+        string title;
+        public string author;
+        public int yearOfPublication;
+        public double price;
+
+        public int CompareTo(Book other)
+        {
+            if (other == null) return 1;
+            return price.CompareTo(other.price);
+        }
+
+        //public int CompareTo(Book other)
+        //{
+        //    if (other == null) return 1;
+        //    return author.CompareTo(other.author);
+        //}
+
+        public Book(string title, string author, int yearOfPublication, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.yearOfPublication = yearOfPublication;
+            this.price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{title}, {author}, {yearOfPublication}, {price} zł";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>();
+
+            books.Add(new Book("Hobbit", "Nowak", 1954, 45.99));
+            books.Add(new Book("Hobbit1", "Pawlak", 2000, 155.99));
+            books.Add(new Book("Hobbit2", "Arbuz", 1943, 788.99));
+            books.Add(new Book("Hobbit3", "Karbuz", 1937, 95.99));
+
+            books.Sort(); // CompareTo()
+            Console.WriteLine("\nLista książek sortowana według ceny: ");
+            foreach (Book book in books)
+                Console.WriteLine(book.ToString());
+
+            // OrderBy
+            Console.WriteLine("\nLista książek sortowana według daty publikacji: ");
+            var sortedByYear = books.OrderBy(book => book.yearOfPublication);
+            foreach (Book book in sortedByYear)
+                Console.WriteLine(book.ToString());
+
+            // OrderByDescending
+            Console.WriteLine("\nLista książek sortowana według awtorów nierosnąco: ");
+            var sortedByAuthor = books.OrderByDescending(book => book.author);
+            foreach (Book book in sortedByAuthor)
+                Console.WriteLine(book.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
