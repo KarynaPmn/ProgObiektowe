afbeb52d7e13b1a69d557c0fefec72ff3fc584cd
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-11-17 18:23:53 +0100

    Update 03_Interface.cs

--- KLASA_3/03_Interface.cs
+++ KLASA_3/03_Interface.cs
@@ -11,37 +11,46 @@ namespace Interface
         void MakeSound();
         void Eat();
     }
+    
     public abstract class Animal : IAnimal
     {
         public string Name { get; set; }
         public int Age { get; set; }
+        
         public Animal(string name, int age)
         {
             Name = name;
             Age = age;
         }
+        
         public abstract void MakeSound();
+        
         public void Eat()
         {
             Console.WriteLine($"{Name} je");
         }
+        
         public class Dog : Animal
         {
             public Dog(string name, int age) : base(name, age) { }
+            
             public override void MakeSound()
             {
                 Console.WriteLine("Gau!");
             }
+            
         }
 
         public class Cat : Animal
         {
             public Cat(string name, int age) : base(name, age) { }
+            
             public override void MakeSound()
             {
                 Console.WriteLine("Meow!");
             }
         }
+        
         internal class Program
         {
             static void Main(string[] args)
@@ -62,6 +71,7 @@ namespace Interface
                     new Dog("Azor", 3),
                     new Cat("Mruczek", 2),
                 };
+                
                 foreach (var animal in Animal)
                 {
                     animal.MakeSound();
