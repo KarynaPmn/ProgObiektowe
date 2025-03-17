2e8b186cb0bb83607743a04cba64cd4ae61b5adb
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2025-03-17 08:41:35 +0100

    Add files via upload

---
+++ KLASA_3/06_2_ZdarzeniaRefaktoryzacja/src/Enumerator/Permission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.src.Enumerator
+{
+    public enum Permission
+    {
+        Read,
+        Write,
+        Delete,
+        ManageUsers
+    }
+}
---
+++ KLASA_3/06_2_ZdarzeniaRefaktoryzacja/src/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp2.src.Enumerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.src.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public List<Role> Roles { get; set; }
+
+        public User(string username)
+        {
+            Username = username;
+            Roles = new List<Role>();
+        }
+
+        public void AddRole(Role role)
+        {
+            if (!Roles.Contains(role))
+            {
+                Roles.Add(role);
+            }
+        }
+    }
+}
