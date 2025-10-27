1a4650003d6b855b2fbbd7bb79b7a7d046fa89f4
Karyna Pomin <115844791+KarynaPmn@users.noreply.github.com>
2025-10-27 20:08:46 +0100

    Update Singleton.cs

--- KLASA_4/Zadanie_1/WzorceProjektowe/Singleton.cs
+++ KLASA_4/Zadanie_1/WzorceProjektowe/Singleton.cs
@@ -11,26 +11,29 @@ namespace WzorceProjektowe
             private static ConfigurationManager _instance;
 
             // dla bezpieczeństwa wątków, tylko jeden wątek naraz
-            private static readonly object _lock = new object(); 
+            //private static readonly object _lock = new object(); 
 
             public string Path { get; private set; }
             public bool DebugMode { get; private set; }
 
             // Tylko sama klasa może wywołać swój konstruktor to umożliwia kontrolę, by istniała tylko jedna instancja
-            private ConfigurationManager() { } 
+            private ConfigurationManager() { }
 
-            public static ConfigurationManager Instance
-            {
-                get
-                {
-                    lock (_lock)
-                    {
-                        if (_instance == null)
-                            _instance = new ConfigurationManager();
-                        return _instance;
-                    }
-                }
-            }
+            public static ConfigurationManager Instance => _instance ??= new ConfigurationManager();
+
+            // ??= 
+            //public static ConfigurationManager Instance
+            //{
+            //    get
+            //    {
+            //        lock (_lock)
+            //        {
+            //            if (_instance == null)
+            //                _instance = new ConfigurationManager();
+            //            return _instance;
+            //        }
+            //    }
+            //}
 
             public void SetConfig(string path, bool debug)
             {
